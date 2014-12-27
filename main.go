e29bfb2c5a35f8548afdc80ebe07359f4e71f9f6
He Liu <liulonnie@gmail.com>
2014-12-27 07:42:33 -0800

    .

--- main.go
+++ main.go
@@ -1,29 +1,192 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
-	"sort"
+	"strings"
+	// "sort"
 
 	"go/build"
+	"go/token"
 	"path/filepath"
 
-	// "golang.org/x/tools/go/buildutil"
 	"golang.org/x/tools/go/loader"
+	"golang.org/x/tools/go/types"
 	"golang.org/x/tools/refactor/lexical"
 )
 
+// File contains dependency info of a file
 type File struct {
-	Depends []string
-	UsedBy  []string
+	Name    string
+	Depends map[string]*File
+	UsedBy  map[string]*File
+
+	hits  map[string]bool
+	level int
 }
 
-type Packcage struct {
-	Deps  []string
-	Files map[string]*File
+// Package contains dependency info of a package.
+type Package struct {
+	Depends []string         // depending packages
+	Files   map[string]*File // files
+
+	LevelsTop [][]*File
+	LevelsBot [][]*File
 }
 
-func Build(packPath string) {
+type reverse bool
+
+func (r reverse) pres(f *File) map[string]*File {
+	if !r {
+		return f.Depends
+	}
+	return f.UsedBy
+}
+
+func (r reverse) posts(f *File) map[string]*File {
+	if !r {
+		return f.UsedBy
+	}
+	return f.Depends
+}
+
+func newFile(name string) *File {
+	ret := new(File)
+	ret.Name = name
+	ret.Depends = make(map[string]*File)
+	ret.UsedBy = make(map[string]*File)
+
+	return ret
+}
+
+func (p *Package) addFile(s string) {
+	p.Files[s] = newFile(s)
+}
+
+func (p *Package) depends(fdef, fused string) {
+	def := p.Files[fdef]
+	use := p.Files[fused]
+	if def == nil || use == nil {
+		panic("file not found")
+	}
+
+	if def.UsedBy[fused] == nil {
+		def.UsedBy[fused] = use
+	}
+	if use.Depends[fdef] == nil {
+		use.Depends[fdef] = def
+	}
+}
+
+// PrintDepends prints the list of dependencies.
+func (p *Package) PrintDepends() {
+	for _, dep := range p.Depends {
+		fmt.Println(dep)
+	}
+}
+
+func (p *Package) PrintFileDeps() {
+	for _, f := range p.Files {
+		fmt.Println(f.Name)
+
+		for _, dep := range f.Depends {
+			fmt.Println("    ", dep.Name)
+		}
+	}
+}
+
+func printLevels(lvls [][]*File) {
+	for i, lvl := range lvls {
+		fmt.Printf("%d:", i)
+		for _, f := range lvl {
+			fmt.Printf(" %s", f.Name)
+		}
+		fmt.Println()
+	}
+}
+
+func (p *Package) PrintLevelsTop() { printLevels(p.LevelsTop) }
+func (p *Package) PrintLevelsBot() {
+	n := len(p.LevelsBot)
+	lvls := make([][]*File, n)
+	for i, lvl := range p.LevelsBot {
+		lvls[n-1-i] = lvl
+	}
+	printLevels(lvls)
+}
+
+var errCircDep = errors.New("has circular dependency")
+
+func (p *Package) makeLevels(r reverse) ([][]*File, error) {
+	var cur []*File
+	var next []*File
+	var ret [][]*File
+
+	for _, f := range p.Files {
+		if len(r.pres(f)) == 0 {
+			cur = append(cur, f)
+			f.level = 0
+		} else {
+			f.hits = make(map[string]bool)
+			f.level = -1
+		}
+	}
+
+	ntotal := 0
+	level := 1
+	for len(cur) > 0 {
+		ret = append(ret, cur)
+		ntotal += len(cur)
+
+		for _, f := range cur {
+			posts := r.posts(f)
+			for _, fref := range posts {
+				if r.pres(fref)[f.Name] == nil {
+					panic("bug")
+				}
+
+				if fref.level >= 0 {
+					return nil, errCircDep
+				}
+
+				wasHit := fref.hits[f.Name]
+				fref.hits[f.Name] = true
+
+				if !wasHit && len(fref.hits) == len(r.pres(fref)) {
+					next = append(next, fref)
+					fref.level = level
+				}
+			}
+		}
+
+		cur = next
+		next = nil
+		level++
+	}
+
+	if ntotal != len(p.Files) {
+		return nil, errCircDep
+	}
+
+	return ret, nil
+}
+
+func (p *Package) MakeLevelsTop() error {
+	var e error
+	p.LevelsTop, e = p.makeLevels(reverse(false))
+	return e
+}
+
+func (p *Package) MakeLevelsBot() error {
+	var e error
+	p.LevelsBot, e = p.makeLevels(reverse(true))
+	return e
+}
+
+// NewPackage analyzes the dependency info of a package.
+func NewPackage(packPath string) (*Package, error) {
 	ctxt := build.Default
 	conf := loader.Config{
 		Build:         &ctxt,
@@ -31,61 +194,91 @@ func Build(packPath string) {
 	}
 
 	if err := conf.ImportWithTests(packPath); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		return nil, err
 	}
 
 	iprog, err := conf.Load()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
+		return nil, err
 	}
 
-	var deps []string
-	fileDeps := make(map[string]bool)
+	ret := new(Package)
+	ret.Files = make(map[string]*File)
 
-	for pkg, info := range iprog.AllPackages {
+	var thePkg *types.Package
+	for pkg := range iprog.AllPackages {
 		name := pkg.Path()
 		if name != packPath {
-			deps = append(deps, name)
+			ret.Depends = append(ret.Depends, name)
+			continue
+		}
+		thePkg = pkg
+	}
+
+	fset := iprog.Fset
+	fname := func(p token.Pos) string {
+		ret := filepath.Base(fset.Position(p).Filename)
+		if strings.HasSuffix(ret, "_test.go") {
+			return strings.TrimSuffix(ret, "_test.go")
+		} else if strings.HasSuffix(ret, ".go") {
+			return strings.TrimSuffix(ret, ".go")
+		}
+		return ret
+	}
+
+	info := iprog.AllPackages[thePkg]
+	for _, f := range info.Files {
+		ret.addFile(fname(f.Package))
+	}
+
+	res := lexical.Structure(iprog.Fset, thePkg, &info.Info, info.Files)
+	for obj, refs := range res.Refs {
+		pack := obj.Pkg()
+		if pack != thePkg {
+			// we only care about internal references
 			continue
 		}
 
-		res := lexical.Structure(iprog.Fset, pkg, &info.Info, info.Files)
-		fset := iprog.Fset
-
-		for obj, refs := range res.Refs {
-			pack := obj.Pkg()
-			if pack != pkg {
-				continue
-			}
-
-			fdef := filepath.Base(fset.Position(obj.Pos()).Filename)
-
-			for _, ref := range refs {
-				fused := filepath.Base(fset.Position(ref.Id.NamePos).Filename)
-				if fused != fdef {
-					s := fdef + " <- " + fused
-					fileDeps[s] = true
-				}
+		fdef := fname(obj.Pos())
+		for _, ref := range refs {
+			fused := fname(ref.Id.NamePos)
+			if fused != fdef {
+				ret.depends(fdef, fused)
 			}
 		}
 	}
 
-	var strs []string
-	for m := range fileDeps {
-		strs = append(strs, m)
-	}
-	sort.Strings(deps)
-	sort.Strings(strs)
+	return ret, nil
+}
 
-	for _, dep := range deps {
-		fmt.Println(dep)
-	}
-	for _, s := range strs {
-		fmt.Println(s)
+func ne(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, "error:", e)
+		os.Exit(-1)
 	}
 }
 
 func main() {
-	Build("lonnie.io/e8vm/asm8")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Fprintln(os.Stderr, "requires exacly one package name")
+		os.Exit(-1)
+	}
+
+	p, e := NewPackage(args[0])
+	ne(e)
+
+	p.PrintFileDeps()
+
+	e = p.MakeLevelsTop()
+	ne(e)
+
+	fmt.Println("From bot to top:")
+	p.PrintLevelsTop()
+
+	fmt.Println("From top to bot:")
+	e = p.MakeLevelsBot()
+	p.PrintLevelsBot()
 }
